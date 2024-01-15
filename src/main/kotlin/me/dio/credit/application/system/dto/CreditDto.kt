a79a7e34d48c96831a3172033dc0e6c89889072e
sergiolinss <Sergiovictor2000outlook.com>
2024-01-15 20:41:58 -0300

    setimo commit

--- src/main/kotlin/me/dio/credit/application/system/dto/CreditDto.kt
+++ src/main/kotlin/me/dio/credit/application/system/dto/CreditDto.kt
@@ -1,6 +1,7 @@
 package me.dio.credit.application.system.dto
 
 import jakarta.validation.constraints.Future
+import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Positive
@@ -11,15 +12,31 @@ import java.time.LocalDate
 
 data class CreditDto(
     @field:NotNull(message = "Income can not be null") val creditValue: BigDecimal,
-    @field:Future val dayFirstOfInstallment: LocalDate,
-    @field:Positive(message = "Min 1 of Installments") val numberOfInstallments: Int,
+    val dayFirstOfInstallment: LocalDate,
+    @field:Min(value = 1, message = "The minimum of installments is 1")
+    @field:Max(value = 48, message = "The maximum of installments is 48")
+    val numberOfInstallments: Int,
     @field:NotNull(message = "ID can not be null") val customerId: Long
 
 ) {
-    fun toEntity(): Credit = Credit(
-        creditValue = this.creditValue,
-        dayFirstInstallment = this.dayFirstOfInstallment,
-        numberOfInstallments = this.numberOfInstallments,
-        customer = Customer(id = this.customerId)
-    )
+    init {
+        validateDateOfFirstInstallments(dayFirstOfInstallment)
+    }
+
+    private fun validateDateOfFirstInstallments(date: LocalDate) {
+        val MaximumDate = LocalDate.now().plusMonths(3)
+
+        if (date.isAfter(MaximumDate)) {
+            throw IllegalArgumentException("The date of the first installment must be at most 3 months from today")
+        }
+
+    }
+
+
+fun toEntity(): Credit = Credit(
+    creditValue = this.creditValue,
+    dayFirstInstallment = this.dayFirstOfInstallment,
+    numberOfInstallments = this.numberOfInstallments,
+    customer = Customer(id = this.customerId)
+)
 }
